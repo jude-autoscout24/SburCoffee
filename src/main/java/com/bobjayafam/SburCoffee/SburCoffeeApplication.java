df9066e4f2e9a15d9cf53f50a62caaceb331bad6
Jude Afam <jude.afam@autoscout24.com>
2023-05-01 09:40:33 +0200

    set up

---
+++ src/main/java/com/bobjayafam/SburCoffee/SburCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.bobjayafam.SburCoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SburCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SburCoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bobjayafam/SburCoffee/SburCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bobjayafam.SburCoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SburCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
