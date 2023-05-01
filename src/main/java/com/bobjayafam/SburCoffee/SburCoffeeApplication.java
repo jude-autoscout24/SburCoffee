5b5bccc93871bd74cd966850c59e3fa84cabf4d9
Jude Afam <jude.afam@autoscout24.com>
2023-05-01 09:51:02 +0200

    add coffee model

--- src/main/java/com/bobjayafam/SburCoffee/SburCoffeeApplication.java
+++ src/main/java/com/bobjayafam/SburCoffee/SburCoffeeApplication.java
@@ -3,6 +3,8 @@ package com.bobjayafam.SburCoffee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class SburCoffeeApplication {
 
@@ -10,4 +12,30 @@ public class SburCoffeeApplication {
 		SpringApplication.run(SburCoffeeApplication.class, args);
 	}
 
+}
+
+class Coffee {
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name){
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId(){
+		return id;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
 }
